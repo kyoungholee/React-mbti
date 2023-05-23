94f382cb2d28950d04a593ae14eafd8fe55ec0a0
kyoungholee <dlrudgh1303@gmail.com>
2023-05-23 23:59:33 +0900

    테스트 페이지 진행중

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../styles/home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <div className={styles.top}>
+            <img
+              className={styles.logo}
+              src="img/hiker-travel.jpg"
+              alt="로고"
+            />
+          </div>
+          <div className={styles.middle}>
+            <h2 className={styles.header}>올해는 어디로 갈까?</h2>
+            <p>총 16개의 유형의 MBTI성향을 기반으로</p>
+            <p>가장 잘 어울리는 여행지를 추천해드려요.</p>
+          </div>
+          <div className={styles.bottom}>
+            <Link to="/test" className={styles.start__button}>
+              테스트 하기
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
