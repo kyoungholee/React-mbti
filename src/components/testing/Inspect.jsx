4c40bea3ee1198893497c115e71ac20bd55e8a1c
kyoungholee <dlrudgh1303@gmail.com>
2023-05-25 00:55:51 +0900

    검사페이지  모던 css 경로 수정

--- src/components/testing/Inspect.jsx
+++ src/components/testing/Inspect.jsx
@@ -1,5 +1,5 @@
 import React, { createRef, useEffect, useState } from "react";
-import styles from "../../styles/inspect.module.css";
+import styles from "../../styles/.module.css";
 import { useNavigate } from "react-router-dom";
 import Questions from "../../common/api/questionApi.json";
 
