fb6de2c1768718867e072f8805bc196f7f527a25
kyoungholee <dlrudgh1303@gmail.com>
2023-05-26 15:45:03 +0900

    동적 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Home from "./components/home/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Test from "./components/testing/Inspect";
+import Countries from "./components/countries/countries";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
-        <Route exact path="/test" element={<Test />}></Route>
+        <Route path="/test" element={<Test />}></Route>
+        <Route path="/result/:nation" element={<Countries />} />
       </Routes>
     </BrowserRouter>
   );
