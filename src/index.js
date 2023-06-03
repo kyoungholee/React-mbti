947085d35dab06d11eabb6c4c753df508f6d652b
kyoungholee <dlrudgh1303@gmail.com>
2023-06-03 15:17:41 +0900

    index.js 초기화

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { hydrate, render } from "react-dom";
 import "./index.css";
 import App from "./App";
+import { HelmetProvider } from "react-helmet-async";
 
-const $root = document.getElementById("root");
-const renderOrHydrate = $root.hasChildNodes() ? hydrate : render;
-
-renderOrHydrate(<App />, $root);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <HelmetProvider>
+        <App />
+        </HelmetProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
